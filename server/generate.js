8f5bee2f7b96e5f2691c8fa63dc42951da38440a
fcamadi <fcamadi@gmail.com>
2020-01-19 17:53:15 +0100

    small change in the json server

--- server/generate.js
+++ server/generate.js
@@ -4,13 +4,14 @@ var faker = require('faker');
 
 var database = { products: []};
 
-for (var i = 1; i<= 300; i++) {
+for (var i = 1; i<= 100; i++) {
   database.products.push({
     id: i,
     name: faker.commerce.productName(),
     description: faker.lorem.sentences(),
     price: faker.commerce.price(),
-    imageUrl: "https://source.unsplash.com/1600x900/?product",
+    //imageUrl: "https://source.unsplash.com/1600x900/?product",
+    imageUrl: "https://source.unsplash.com/800x600/?product"+i,
     quantity: faker.random.number()
   });
 }
